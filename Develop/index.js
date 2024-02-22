9ad97bcc291743eb7646e505530d5140e2b1e44b
Jesse <jessecdenier@gmail.com>
2024-02-22 13:36:00 -0600

    Ran npm Init and installed npm Inquirer8.2.4

--- Develop/index.js
+++ Develop/index.js
@@ -1,7 +1,18 @@
 // TODO: Include packages needed for this application
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+  "What does your project do?",
+  "Why did you create your project?",
+  "How does your project work?",
+  "Step by step. What needs to be done to install and run your project?",
+  "How does the end-user use your project?",
+  "What are some use case examples for your project?",
+  "Did anyone else work on your project?",
+  "Did you use any third-party asses when creating your project?",
+  "Did you follow any tutorials when creating your project?",
+  "Will you be using the standard MIT lisense or something else?",
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
