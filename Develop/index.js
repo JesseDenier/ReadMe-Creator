0641aaac9e9934f6d8f9359bed9e48ddf19429b7
Jesse <jessecdenier@gmail.com>
2024-02-22 14:02:03 -0600

    Added inquirer to index.js and started building out questions with it.

--- Develop/index.js
+++ Develop/index.js
@@ -1,18 +1,58 @@
-// TODO: Include packages needed for this application
+console.log(`--------------------index.js has started.--------------------`);
 
-// TODO: Create an array of questions for user input
-const questions = [
-  "What does your project do?",
-  "Why did you create your project?",
-  "How does your project work?",
-  "Step by step. What needs to be done to install and run your project?",
-  "How does the end-user use your project?",
-  "What are some use case examples for your project?",
-  "Did anyone else work on your project?",
-  "Did you use any third-party asses when creating your project?",
-  "Did you follow any tutorials when creating your project?",
-  "Will you be using the standard MIT lisense or something else?",
-];
+// Loads the npm inquirer and file system packages into this javascript and allows them to be called.
+const inquirer = require('inquirer');
+const fs = require('fs');
+
+// A series of questions that will appear in the console.
+// TODO: Currently these answers just appear as a console log at the end and then are not saved or added to a README.
+inquirer
+  .prompt([
+    {
+      name: "description1",
+      message: "What does your project do?",
+    },
+    {
+      name: "description2",
+      message: "Why did you create your project?",
+    },
+    {
+      name: "description3",
+      message: "How does your project work?",
+    },
+    {
+      name: "installation",
+      message:
+        "Step by step. What needs to be done to install and run your project?",
+    },
+    {
+      name: "usage1",
+      message: "How does the end-user use your project?",
+    },
+    {
+      name: "usage2",
+      message: "What are some use case examples for your project?",
+    },
+    {
+      name: "credits1",
+      message: "Did anyone else work on your project?",
+    },
+    {
+      name: "credits2",
+      message: "Did you use any third-party asses when creating your project?",
+    },
+    {
+      name: "credits3",
+      message: "Did you follow any tutorials when creating your project?",
+    },
+    {
+      name: "license",
+      message: "Will you be using the standard MIT lisense or something else?",
+    },
+  ])
+  .then((answers) => {
+    console.info("Answer:", answers);
+  });
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
@@ -22,3 +62,5 @@ function init() {}
 
 // Function call to initialize app
 init();
+
+//console.log(`---------------------index.js has ended.---------------------`);
